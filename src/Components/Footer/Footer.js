a94990522d79b37f6651ae32e216808bd22d5280
Nafim16 <dnafim081999@gmail.com>
2023-07-23 14:02:55 +0600

    6th

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import './Footer.css';
+
 
 const Footer = () => {
     return (
         <div>
             <footer>
-                <h3>this is footer</h3>
+                <h1 className='foot-h1'>Leading University Alumni</h1>
+
+                <h4 className='foot-h4'>Ground Floor - Leading University Information Center <br />
+
+                    Phone: President - 017333577217; <br /> General Secretary - 01819311815 <br />
+
+                    Other Contacts: 01312181166, 01842181166 <br />
+
+                    Technical Support: 01742834880 <br />
+
+                    Email: <a href="https://www.lus.ac.bd" >https://www.lus.ac.bd</a></h4>
+
+                <div class="foot">
+                    <a ><i class="fa-brands fa-facebook"></i></a>
+                    <a ><i class="fa-brands fa-whatsapp"></i></a>
+                    <a ><i class="fa-brands fa-instagram"></i></a>
+
+                </div>
+
             </footer>
+
         </div>
     );
 };
--- src/Components/Whyjoinus/Whyjoinus.js
+++ src/Components/Whyjoinus/Whyjoinus.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Whyjoinus.css';
 import { useNavigate } from 'react-router-dom';
+import job from '../img/job.png';
+import event from '../img/event.png'
+import connect from '../img/connect.png'
+
 const Whyjoinus = () => {
 
     const navigate = useNavigate();
@@ -15,7 +19,9 @@ const Whyjoinus = () => {
                     <h1 className='text-center mb-4'>Here is why you should join us</h1>
                     <div className='row p-4'>
                         <div className='col-md-4'>
-                            <div className="card text-white bg-secondary mb-3 p-3 shadow">
+                            <div className="card text-white bg mb-3 p-3 shadow">
+                                {/* <img src={job} className="card-img-top" /> */}
+                                <img src={connect} className='card-img-top image1' />
                                 <div className="card-header">Reconnect</div>
                                 <div className="card-body">
                                     <p className="card-text">Reconnect with the members of CSE Alumni LU in the secured, ad-free online Network</p>
@@ -23,7 +29,8 @@ const Whyjoinus = () => {
                             </div>
                         </div>
                         <div className='col-md-4'>
-                            <div className="card text-white bg-secondary mb-3 p-3 shadow">
+                            <div className="card text-white bg mb-3 p-3 shadow">
+                                <img src={event} className='card-img-top image1' />
                                 <div className="card-header">Give Back</div>
                                 <div className="card-body">
                                     <p className="card-text">Give Back to various causes through fundraising events.</p>
@@ -31,7 +38,8 @@ const Whyjoinus = () => {
                             </div>
                         </div>
                         <div className='col-md-4'>
-                            <div className="card text-white bg-secondary mb-3 p-3 shadow">
+                            <div className="card text-white bg mb-3 p-3 shadow">
+                                <img src={job} className='card-img-top image1' />
                                 <div className="card-header">Advance</div>
                                 <div className="card-body">
                                     <p className="card-text">Advance your career, post job opportunities, and find open positions in your professional network.</p>
