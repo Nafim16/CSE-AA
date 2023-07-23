9db98383b03a87792f10e98792514663f5ae0345
Nafim16 <dnafim081999@gmail.com>
2023-07-23 21:09:26 +0600

    auto sliding fixed

--- src/Components/Carouse_event/Event.js
+++ src/Components/Carouse_event/Event.js
@@ -12,21 +12,21 @@ const Event = () => {
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
+                    <div className="carousel-item active" data-bs-interval="5000">
                         <img src={loginarea} className="d-block w-100" alt="..."/>
                             <div className="carousel-caption d-none d-md-block d-sm-block">
                                 <h5>First slide label</h5>
                                 <p>Some representative placeholder content for the first slide.</p>
                             </div>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="5000">
                         <img src={sohidminar3} className="d-block w-100" alt="..."/>
                             <div className="carousel-caption d-none d-md-block d-sm-block">
                                 <h5>Second slide label</h5>
                                 <p>Some representative placeholder content for the second slide.</p>
                             </div>
                     </div>
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="5000">
                         <img src={loginarea} className="d-block w-100" alt="..."/>
                             <div className="carousel-caption d-none d-md-block d-sm-block">
                                 <h5>Third slide label</h5>
