7a3ac865370f7dbed16e855de3e484788a1a9aa3
Nafim16 <dnafim081999@gmail.com>
2023-12-26 22:04:57 +0600

    job post creation added

--- src/Components/Job/JobCreate.js
+++ src/Components/Job/JobCreate.js
@@ -14,23 +14,23 @@ const JobCreate = () => {
                     <div className='formfield'>
                         <form action="">
                             <div className="form-floating mb-3">
-                                <input type="email" className="form-control" id="floatingInputDisabled" />
+                                <input type="email" className="form-control" placeholder='abc' id="floatingInputDisabled" />
                                 <label htmlFor="floatingInputDisabled">Company</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="floatingTextareaDisabled" />
+                                <input className="form-control" id="floatingTextareaDisabled" placeholder='abc' />
                                 <label htmlFor="floatingTextareaDisabled">Job Title</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="floatingTextarea2Disabled" />
+                                <input className="form-control" id="floatingTextarea2Disabled" placeholder='abc' />
                                 <label htmlFor="floatingTextarea2Disabled">Position</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="floatingTextarea2Disabled" />
+                                <input className="form-control" id="floatingTextarea2Disabled" placeholder='abc' />
                                 <label htmlFor="floatingTextarea2Disabled">Job Location</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input className="form-control" id="floatingTextarea2Disabled" />
+                                <input className="form-control" id="floatingTextarea2Disabled" placeholder='abc' />
                                 <label htmlFor="floatingTextarea2Disabled">Job Description</label>
                             </div>
                             <button class="btn btn-primary " type="submit">Post Job</button>
